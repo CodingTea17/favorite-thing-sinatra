3c3a48a9b35fbba5b746cb88ac03d891b7d3a650
Dawson Mortenson and John Michael <john@>
2017-09-13 09:18:12 -0700

    code for the favorite things list maker

--- app.rb
+++ app.rb
@@ -1,5 +1,23 @@
 require('sinatra')
 require('sinatra/reloader')
 also_reload('lib/**/*.rb')
-require('./lib/list')
+require('./lib/item')
 require('pry')
+
+get('/') do
+  @list = Item.all()
+  erb(:list)
+end
+
+post('/') do
+  name = params["name"]
+  item = Item.new(name)
+  item.save()
+  @list = Item.all()
+  erb(:list)
+end
+
+get('/items/:id') do
+  @item = Item.find(params[:id])
+  erb(:item)
+end
---
+++ spec/item_spec.rb
@@ -0,0 +1,45 @@
+require('item')
+
+RSpec.describe("Item") do
+  before() do
+   Item.clear()
+  end
+
+  describe(".clear") do
+    it("clears all items from the list") do
+      item = Item.new("tacos")
+      item.save()
+      Item.clear()
+      expect(Item.all()).to(eq([]))
+    end
+  end
+
+  describe(".all") do
+    it("is empty at first") do
+      expect(Item.all()).to(eq([]))
+    end
+  end
+
+  describe("#save") do
+    it("saves an item to the list of items") do
+      item = Item.new("tacos")
+      item.save()
+      expect(Item.all()).to(eq([item]))
+    end
+  end
+end
+
+RSpec.describe("#id") do
+  before() do
+   Item.clear()
+  end
+
+  it("increments an id by 1 each time a new item is added") do
+    item = Item.new("tacos")
+    item.save()
+    item2 = Item.new("pizza")
+    item2.save()
+    expect(item.id()).to eq(1)
+    expect(item2.id()).to eq(2)
+  end
+end
